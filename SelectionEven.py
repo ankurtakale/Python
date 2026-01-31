49a878c5a346051a6ada9e8c1a6ca54df7e4c50c
Ankur Takale <takaleankur@gmail.com>
2026-01-31 22:15:50 +0530

    Python

---
+++ SelectionEven.py
@@ -0,0 +1,8 @@
+# Scripting Approach
+
+No = 22
+
+if (No % 2 == 0):
+    print("It is even")
+else:
+    print("It is odd")
---
+++ SelectionEvenFunctionXX.py
@@ -0,0 +1,15 @@
+# Procedural
+
+def CheckEven(No):
+    if (No % 2 == 0):
+        print("It is even")
+    else:
+        print("It is odd")
+
+Value = 0
+
+print("Enter number : ")
+Value = int(input())
+
+CheckEven(Value)
+
