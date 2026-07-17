59065fbe86ef1f6338db97719f22dcfba73390ba
ankurtakale <takaleankur@gmail.com>
2026-07-17 21:47:47 +0530

    automation

---
+++ DirectoryAutomationEmptyDeleteReportLogTimeStamp.py
@@ -0,0 +1,73 @@
+import sys
+import os
+import time
+
+def DirectoryScanner(DirName = "Marvellous"):
+    Border = "-"*50
+    Timestamp = time.ctime()
+
+    LogFileName = "Marvellous%s.log" %(Timestamp)
+    LogFileName = LogFileName.replace(" ","_")
+    LogFileName = LogFileName.replace(":","_")
+
+    fobj = open(LogFileName,"w")
+
+    fobj.write(Border+"\n")
+    fobj.write("This is a log file created by Marvellous Automation\n")
+    fobj.write("This is a directory cleaner script\n")
+    fobj.write(Border+"\n")
+
+    Ret = False
+
+    Ret = os.path.exists(DirName)
+    if(Ret == False):
+        print("There is no such directory")
+        return
+    
+    Ret = os.path.isdir(DirName)
+    if(Ret == False):
+        print("It is not a directory")
+        return
+    
+    FileCount = 0
+    EmptyFileCount = 0
+
+    for FolderName, SubFolder, FileName in os.walk(DirName):
+        
+        for fname in FileName:
+            FileCount = FileCount + 1
+
+            fname = os.path.join(FolderName,fname)
+
+            if(os.path.getsize(fname) == 0):        # Empty file
+                EmptyFileCount = EmptyFileCount + 1
+                os.remove(fname)
+
+    fobj.write("Total files scanned : "+str(FileCount)+"\n")
+    fobj.write("Total empty files found : "+str(EmptyFileCount)+"\n")
+    fobj.write("This log file is created at : "+Timestamp+"\n")
+    fobj.write(Border+"\n")
+
+    fobj.close()
+
+def main():
+    Border = "-"*50
+    print(Border)
+    print("-------- Marvellous Directory Automation ---------")
+    print(Border)
+
+    if(len(sys.argv) != 2):
+        print("Invalid number of arguments")
+        print("Please specify the name of directory")
+        return
+    
+    DirectoryScanner(sys.argv[1])
+
+    print(Border)
+    print("-------- Marvellous Directory Automation ---------")
+    print(Border)
+
+
+
+if __name__ == "__main__":
+    main()
